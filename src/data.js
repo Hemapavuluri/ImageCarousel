d5abe9e5ad3a8d3042dc31cd440741b6d935b6e0
Hemapavuluri <hema.pavuluri@gmail.com>
2018-10-14 23:34:10 +0200

    Add fake data to display

---
+++ src/data.js
@@ -0,0 +1,202 @@
+import Image1 from "./images/Image1.jpg";
+import Image2 from "./images/Image2.jpg";
+import Image3 from "./images/Image3.jpg";
+import Image4 from "./images/Image4.jpg";
+import Image5 from "./images/Image5.jpg";
+import Image6 from "./images/Image6.jpg";
+import Image7 from "./images/Image7.jpg";
+import Image8 from "./images/Image8.jpeg";
+import Image9 from "./images/Image9.jpeg";
+import Image10 from "./images/Image10.jpeg";
+import Image11 from "./images/Image11.jpeg";
+import Image12 from "./images/Image12.jpeg";
+import Image13 from "./images/Image13.jpeg";
+import Image14 from "./images/Image14.jpeg";
+import Image15 from "./images/Image15.jpeg";
+
+const data = {
+    "properties": [
+    {
+        "index": 0,
+        "price": 937180,
+        "picture": `${Image1}`,
+        "city": "Singer",
+        "address": "914 Argyle Road",
+        "latitude": -33.944576,
+        "longitude": 151.25584,
+        "bedrooms": 2,
+        "bathrooms": 2,
+        "carSpaces": 2
+    },
+    {
+        "index": 1,
+        "price": 703725,
+        "picture": `${Image2}`,
+        "city": "Machias",
+        "address": "255 Raleigh Place",
+        "latitude": -33.944471,
+        "longitude": 151.2541,
+        "bedrooms": 2,
+        "bathrooms": 1,
+        "carSpaces": 0
+    },
+    {
+        "index": 2,
+        "price": 837111,
+        "picture": `${Image3}`,
+        "city": "Bend",
+        "address": "580 Amber Street",
+        "latitude": -33.944644,
+        "longitude": 151.24834,
+        "bedrooms": 3,
+        "bathrooms": 2,
+        "carSpaces": 0
+    },
+    {
+        "index": 3,
+        "price": 648223,
+        "picture": `${Image4}`,
+        "city": "Ivanhoe",
+        "address": "854 Varanda Place",
+        "latitude": -33.940396,
+        "longitude": 151.2469,
+        "bedrooms": 3,
+        "bathrooms": 2,
+        "carSpaces": 0
+    },
+    {
+        "index": 4,
+        "price": 771826,
+        "picture": `${Image5}`,
+        "city": "Lydia",
+        "address": "857 Rockaway Parkway",
+        "latitude": -33.944562,
+        "longitude": 151.2503,
+        "bedrooms": 2,
+        "bathrooms": 1,
+        "carSpaces": 1
+    },
+    {
+        "index": 5,
+        "price": 686238,
+        "picture": `${Image6}`,
+        "city": "Rosedale",
+        "address": "543 Harman Street",
+        "latitude": -33.939304,
+        "longitude": 151.25046,
+        "bedrooms": 2,
+        "bathrooms": 1,
+        "carSpaces": 1
+    },
+    {
+        "index": 6,
+        "price": 595377,
+        "picture": `${Image7}`,
+        "city": "Allison",
+        "address": "787 Gerald Court",
+        "latitude": -33.937819,
+        "longitude": 151.25774,
+        "bedrooms": 2,
+        "bathrooms": 2,
+        "carSpaces": 2
+    },
+    {
+        "index": 7,
+        "price": 696189,
+        "picture": `${Image8}`,
+        "city": "Nicut",
+        "address": "416 Vandervoort Avenue",
+        "latitude": -33.943154,
+        "longitude": 151.24687,
+        "bedrooms": 4,
+        "bathrooms": 1,
+        "carSpaces": 1
+    },
+    {
+        "index": 8,
+        "price": 716114,
+        "picture": `${Image9}`,
+        "city": "Canterwood",
+        "address": "437 Mill Street",
+        "latitude": -33.943723,
+        "longitude": 151.25278,
+        "bedrooms": 3,
+        "bathrooms": 1,
+        "carSpaces": 0
+    },
+    {
+        "index": 9,
+        "price": 739852,
+        "picture": `${Image10}`,
+        "city": "Cobbtown",
+        "address": "553 Friel Place",
+        "latitude": -33.942459,
+        "longitude": 151.24941,
+        "bedrooms": 2,
+        "bathrooms": 1,
+        "carSpaces": 1
+    },
+    {
+        "index": 10,
+        "price": 625917,
+        "picture": `${Image11}`,
+        "city": "Blue",
+        "address": "687 Opal Court",
+        "latitude": -33.939606,
+        "longitude": 151.25289,
+        "bedrooms": 4,
+        "bathrooms": 1,
+        "carSpaces": 2
+    },
+    {
+        "index": 11,
+        "price": 591236,
+        "picture": `${Image12}`,
+        "city": "Craig",
+        "address": "183 Canton Court",
+        "latitude": -33.938557,
+        "longitude": 151.24773,
+        "bedrooms": 2,
+        "bathrooms": 1,
+        "carSpaces": 0
+    },
+    {
+        "index": 12,
+        "price": 995539,
+        "picture": `${Image13}`,
+        "city": "Orovada",
+        "address": "234 Prospect Street",
+        "latitude": -33.938345,
+        "longitude": 151.25045,
+        "bedrooms": 1,
+        "bathrooms": 2,
+        "carSpaces": 0
+    },
+    {
+        "index": 13,
+        "price": 866924,
+        "picture": `${Image14}`,
+        "city": "Clay",
+        "address": "205 Ocean Parkway",
+        "latitude": -33.937763,
+        "longitude": 151.24721,
+        "bedrooms": 1,
+        "bathrooms": 1,
+        "carSpaces": 2
+    },
+    {
+        "index": 14,
+        "price": 810745,
+        "picture": `${Image15}`,
+        "city": "Windsor",
+        "address": "324 Degraw Street",
+        "latitude": -33.938662,
+        "longitude": 151.2513,
+        "bedrooms": 2,
+        "bathrooms": 2,
+        "carSpaces": 2
+    }
+    ]
+}
+
+export default data;
