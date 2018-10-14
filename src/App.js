0f438447606055afcbfef4536c32f25cbdea1d6e
Hemapavuluri <hema.pavuluri@gmail.com>
2018-10-15 00:51:37 +0200

    Add next and prev buttons to display cards accordingly

--- src/App.js
+++ src/App.js
@@ -8,18 +8,47 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      properties: data.properties
+      properties: data.properties,
+      property: data.properties[0]
     }
   }
 
+  nextProperty = () => {
+  const newIndex = this.state.property.index+1;
+  this.setState({
+    property: data.properties[newIndex]
+  })
+}
+
+prevProperty = () => {
+  const newIndex = this.state.property.index-1;
+  this.setState({
+    property: data.properties[newIndex]
+  })
+}
+
   render() {
-    const { properties } = this.state;
+    const { properties, property } = this.state;
     return (
       <div className="App">
         <div className="col">
           {
-            properties.map(property => <Card key={property.index} property={property} />)
+            <Card key={property.index} property={property} />
           }
+        </div>
+        <div className="buttons">
+          <button
+            onClick={() => this.nextProperty()}
+            disabled={property.index === data.properties.length-1}
+          >
+            Next
+          </button>
+          <button
+            onClick={() => this.prevProperty()}
+            disabled={property.index === 0}
+          >
+            Prev
+          </button>
         </div>
       </div>
     );
