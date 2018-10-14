7a28d4b0132ec6912324a112010494ae93d32925
Hemapavuluri <hema.pavuluri@gmail.com>
2018-10-15 00:04:34 +0200

    Loop through each property to display cards with data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card';
+import data from './data'
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      properties: data.properties
+    }
+  }
+
   render() {
+    const { properties } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Image Carousel App
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          {
+            properties.map(property => <Card key={property.index} property={property} />)
+          }
+        </div>
       </div>
     );
   }
