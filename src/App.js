cbf05f25d300af8dd55a7f4c0675eb8a2200680d
Hemapavuluri <hema.pavuluri@gmail.com>
2018-10-15 09:59:08 +0200

    fix all eslint issues

--- src/App.js
+++ src/App.js
@@ -4,62 +4,68 @@ import Card from './Card';
 import data from './data';
 
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       properties: data.properties,
-      property: data.properties[0]
-    }
+      property: data.properties[0],
+    };
   }
 
   nextProperty = () => {
-  const newIndex = this.state.property.index+1;
-  this.setState({
-    property: data.properties[newIndex]
-  })
-}
+    const { property } = this.state;
+    const newIndex = property.index + 1;
+    this.setState({
+      property: data.properties[newIndex],
+    });
+  }
 
 prevProperty = () => {
-  const newIndex = this.state.property.index-1;
+  const { property } = this.state;
+  const newIndex = property.index - 1;
   this.setState({
-    property: data.properties[newIndex]
-  })
+    property: data.properties[newIndex],
+  });
 }
 
-  render() {
-    const { properties, property } = this.state;
-    return (
-      <div className="App">
-        <div className="col">
-          <div className="cards-slider">
-            <div className="cards-slider-wrapper" style={{
-                  'transform': `translateX(-${property.index*(100/properties.length)}%)`
-                }}>
+render() {
+  const { properties, property } = this.state;
+  return (
+    <div className="App">
+      <div className="col">
+        <div className="cards-slider">
+          <div
+            className="cards-slider-wrapper"
+            style={{
+              transform: `translateX(-${property.index * (100 / properties.length)}%)`,
+            }}
+          >
             {
-              properties.map(property => <Card key={property.index} property={property} />)
+              properties.map(eachProperty => <Card key={eachProperty.index} property={property} />)
             }
-            </div>
           </div>
         </div>
-        <div className="buttons">
-          <button
-            onClick={() => this.nextProperty()}
-            disabled={property.index === data.properties.length-1}
-            className={`${property.index === data.properties.length-1 ? 'disabled' : ''}`}
-          >
-            Next
-          </button>
-          <button
-            onClick={() => this.prevProperty()}
-            className={`${property.index === 0 ? 'disabled' : ''}`}
-          >
-            Prev
-          </button>
-        </div>
       </div>
-    );
-  }
+      <div className="buttons">
+        <button
+          type="button"
+          onClick={() => this.nextProperty()}
+          disabled={property.index === data.properties.length - 1}
+          className={`${property.index === data.properties.length - 1 ? 'disabled' : ''}`}
+        >
+          Next
+        </button>
+        <button
+          type="button"
+          onClick={() => this.prevProperty()}
+          className={`${property.index === 0 ? 'disabled' : ''}`}
+        >
+          Prev
+        </button>
+      </div>
+    </div>
+  );
+}
 }
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import FontAwesome from 'react-fontawesome';
-import PropTypes from 'prop-types';
 import './Card.scss';
 
-const Card = ({property}) => {
-    const {index, picture, city, address, bedrooms, bathrooms, carSpaces} = property;
-    return (
-        <div id={`card-${index}`} className="card">
-            <img src={picture} alt={city} />
-            <div className="details">
-                <span className="index">{index+1}</span>
-                <p className="location">
-                    {city}<br />
-                    {address}
-                </p>
-                <ul className="features">
-                    <li><FontAwesome name='bed' /><span>{bedrooms}</span></li>
-                    <li><FontAwesome name='bath'/><span>{bathrooms}</span></li>
-                    <li><FontAwesome name='car' /><span>{carSpaces}</span></li>
-                </ul>
-            </div>
-        </div>
-    )
-}
-
-Card.propTypes = {
-    property: PropTypes.object.isRequired
-}
+const Card = ({ property }) => {
+  const {
+    index, picture, city, address, bedrooms, bathrooms, carSpaces,
+  } = property;
+  return (
+    <div id={`card-${index}`} className="card">
+      <img src={picture} alt={city} />
+      <div className="details">
+        <span className="index">{index + 1}</span>
+        <p className="location">
+          {city}
+          <br />
+          {address}
+        </p>
+        <ul className="features">
+          <li>
+            <FontAwesome name="bed" />
+            <span>{bedrooms}</span>
+          </li>
+          <li>
+            <FontAwesome name="bath" />
+            <span>{bathrooms}</span>
+          </li>
+          <li>
+            <FontAwesome name="car" />
+            <span>{carSpaces}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
